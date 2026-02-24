4287ff85ea35fe96a093e5c3abcf6b2295a6e9bc
bharathipriyansb <bharathipriyansb@gmail.com>
2026-02-24 22:09:56 +0530

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM Client Work Navigation', () => {
+  test('placeholder', async ({ page }) => {
+    // TODO: implement test steps
+  });
+});
