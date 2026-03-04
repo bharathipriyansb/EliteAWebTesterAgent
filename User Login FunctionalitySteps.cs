c9330dfea990dfaa02a3fb8c34097c29bb08ac6e
bharathipriyansb <bharathipriyansb@gmail.com>
2026-03-04 19:06:00 +0530

    Create User Login FunctionalitySteps.cs

---
+++ User Login FunctionalitySteps.cs
@@ -0,0 +1,22 @@
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+[Binding]
+public class LoginSteps
+{
+    private IWebDriver driver;
+
+    [Given(@"I navigate to the login page")]
+    public void GivenINavigateToTheLoginPage()
+    {
+        driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://example.com/login");
+    }
+
+    [When(@"I enter username ""([^"]*)""")]
+    public void WhenIEnterUsername(string username)
+    {
+        driver.FindElement(By.Id("username")).SendKeys(username);
+    }
+}
